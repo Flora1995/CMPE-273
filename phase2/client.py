765f45e591de9e53d0f9dbe3e7b91dd3e1f0073b
flora <xiaorui.zhang@sjsu.edu>
2020-12-03 23:44:11 -0800

    phase2

--- phase2/client.py
+++ phase2/client.py
@@ -1,11 +1,19 @@
 from flask import Flask, request
 import consul
+import zmq
+import time
+import sys
+from itertools import cycle
+
+import hrw_hashing
+import consistent_hashing
 
 app = Flask(__name__)
 c = consul.Consul(host="127.0.0.1", port="8500")
-producers = {}
+servers = []
 
 def create_clients(servers):
+    producers = {}
     context = zmq.Context()
     for server in servers:
         print(f"Creating a server connection to {server}...")
@@ -13,6 +21,53 @@ def create_clients(servers):
         producer_conn.bind(server)
         producers[server] = producer_conn
     return producers
+
+def generate_data_round_robin(servers):
+    print("Starting...")
+    producers = create_clients(servers)
+    pool = cycle(producers.values())
+    for num in range(10):
+        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
+        print(f"Sending data:{data}")
+        next(pool).send_json(data)
+        time.sleep(1)
+    print("Done")
+
+
+def generate_data_consistent_hashing(servers):
+    print("Starting...")
+    ## TODO
+    producers = create_clients(servers)
+    nodes = []
+    for key in producers.keys():
+        nodes.append(consistent_hashing.Node(producers[key], key))
+    client_ring = consistent_hashing.ConsistentHash(nodes)
+    client_ring.setKeys()
+
+    for num in range(10):
+        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
+        print(f"Sending data:{data}")
+        node = client_ring.get_node(data['key'])
+        node.node.send_json(data)
+        time.sleep(1)
+    print("Done")
+
+def generate_data_hrw_hashing(servers):
+    print("Starting...")
+    ## TODO
+    producers = create_clients(servers)
+    nodes = []
+    weight = 1
+    for key in producers.keys():
+        nodes.append(hrw_hashing.Node(producers[key], weight))
+        weight = weight + 1
+    for num in range(10):
+        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
+        print(f"Sending data:{data}")
+        node = hrw_hashing.determine_responsible_node(nodes, data['key'])
+        node.node.send_json(data)
+        time.sleep(1)
+    print("Done")
 
 
 @app.route('/put', methods=['PUT'])
@@ -41,5 +96,14 @@ def get_all():
     return {"Collection": res}
 
 if __name__ == '__main__':
+    num = 1
+    if len(sys.argv) > 1:
+        num = int(sys.argv[1])
+        print(f"num_server={num}")
+
+    for server in range(num):
+        server_port = "200{}".format(server)
+        servers.append(f'tcp://127.0.0.1:{server_port}')
     app.run(host='127.0.0.1', port=5000)
+
 
--- phase2/server.py
+++ phase2/server.py
@@ -2,33 +2,38 @@ import consul
 import sys
 
 c = consul.Consul()
+storage_dict = {}
+node_num = int(sys.argv[1])
 
-class Server:
-    def _init_(self, port):
-        self.di = {}
-        self.di["collection"] = []
-        self.port = port
+def server(port):
+    context = zmq.Context()
+    consumer = context.socket(zmq.PULL)
+    consumer.connect(f"tcp://127.0.0.1:{port}")
 
-    def server(self):
-        context = zmq.Context()
-        consumer = context.socket(zmq.REP)
-        consumer.bind(f"tcp://127.0.0.1:{self.port}")
-        while True:
-            raw = consumer.recv_json()
-            op = raw['op']
-            if (op == "PUT"):
-                key, value = raw['key'], raw['value']
-                print(f"Server_port={port}:key={key},value={value}")
-                self.put(key, value)
-            if (op == "GET_ONE"):
-                key = raw['key']
-                res = self.get_key(key)
-            if (op == "GET_ALL"):
-                res = self.get_all()
-            if (op == "DE_REG"):
-                self.deregister(self.port)
-            print(f"Server_port={self.port}:key={key},value={value},op={op}")
-            consumer.send_json(res)
+    while True:
+        raw = consumer.recv_json()
+        op = raw['op']
+        if (op == "PUT"):
+            key, value = raw['key'], raw['value']
+            print(f"Server_port={port}:key={key},value={value}")
+            storage_dict["key"] = value
+        if (op == "GET_ONE"):
+            key = raw['key']
+            consumer.send_json({
+                'key': key,
+                'value': storage_dict["key"]
+            })
+        if (op == "GET_ALL"):
+            res = []
+            for key,value in storage_dict.items():
+                res.append({"key":key, "value":value})
+            consumer.send_json({"collections":res})
+        if (op == "Add_Node"):
+            c.agent.service.register(name='server-'+node_num,service_id=node_num, port="200{}".format(node_num))
+            node_num = node_num + 1
+        if (op == "Delete_Node"):
+            node_num = node_num - 1
+            c.agent.service.deregister(name='server-'+node_num,service_id=node_num, port="200{}".format(node_num))    
 
 def register_server(server):
     c.agent.service.register(
@@ -38,16 +43,5 @@ def register_server(server):
     )
 
 if __name__ == "__main__":
-    num_server = 1
-    if len(sys.argv) > 1:
-        num_server = int(sys.argv[1])
-        #print(f"num_server={num_server}")
-
-    for each_server in range(num_server):
-        port = "200{}".format(each_server)
-        #print(f"Starting a server at:{port}...")
-        current_server = {
-            'server': f"tcp://127.0.0.1:{port}",
-            'port': port
-        }
-        register_server(current_server)
+    for index in range(int(sys.argv[1]) - 1):
+        c.agent.service.register(name='server-'+index,service_id=index, port="200{}".format(index))
